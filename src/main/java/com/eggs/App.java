57564e237b9a016d470b8259318a8209e947d8fd
lalyos <lalyos@yahoo.com>
2013-10-17 11:42:10 +0200

    YAML modifications removed

--- src/main/java/com/eggs/App.java
+++ src/main/java/com/eggs/App.java
@@ -12,16 +12,6 @@ public class App {
 		MenuRepository repo = new InmemoryMenuRepository();
 		ConsoleMenuPrinter printer = new ConsoleMenuPrinter(repo);
 		
-		//printer.printMenus();
-		DumperOptions options = new DumperOptions();
-	    options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-	    Yaml yml = new Yaml(options);
-	    
-	    Yaml yml2 = new Yaml(new Constructor(Food.class));
-	    
-	    Object obj = repo.getAllmenu().get(0).getFoodList();
-	    System.out.println(yml2.dump(obj));
-	    //System.out.println(yml2.dump(repo.getAllmenu().get(0)));
-	    
+	    printer.printMenus();
 	}
 }
