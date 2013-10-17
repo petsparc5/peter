5b6b4fa8a6b630139685bed3d286cff33482518e
lalyos <lalyos@yahoo.com>
2013-10-17 15:50:46 +0200

    [csv] switching to CSV Implementation from InMemory implementaion

--- src/main/java/com/eggs/App.java
+++ src/main/java/com/eggs/App.java
@@ -5,11 +5,12 @@ import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import com.eggs.impl.ConsoleMenuPrinter;
+import com.eggs.impl.CsvFileMenuRepository;
 import com.eggs.impl.InmemoryMenuRepository;
 
 public class App {
 	public static void main(String[] args) {
-		MenuRepository repo = new InmemoryMenuRepository();
+		CsvFileMenuRepository repo = new CsvFileMenuRepository("karcsi", "marcello");
 		ConsoleMenuPrinter printer = new ConsoleMenuPrinter(repo);
 		
 	    printer.printMenus();
